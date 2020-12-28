02e8b2c07fe12903719dc8a55179e47378c651b5
Luciana <arevalolucianadg@gmail.com>
2020-12-28 20:31:53 -0300

    Instalo Axios para trabajar con API OpenWeatherMap + Instalo Dotenv para guardar apiKey en variables de entorno

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+const axios = require("axios");
+const dotenv = require("dotenv").config();
+
 const argv = require("./config/yargs").argv;
 
-console.log(argv.city);
+const encodedCity = encodeURI(argv.city);
+const apiKey = process.env.OWM_API_KEY;
+
+const instance = axios.create({
+  baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${encodedCity}&appId=${apiKey}`,
+});
+
+instance
+  .get()
+  .then((resp) => console.log(resp.data))
+  .catch((err) => console.log(err));
